2a437f5db0e18b2173caefbd8bcaafdf17bfe02c
Otto Lukacs <ottolukacs@ottolukacs.com>
2025-06-30 15:31:05 +0200

    Refactor Docker installation in Ansible playbook and update jumpstart script to set hostname correctly

--- iac/jumpstart.sh
+++ iac/jumpstart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#hostnamectl set-hostname blocks-srv
 
+hostnamectl set-hostname blocks-srv
 apt update
 apt install software-properties-common -y
 add-apt-repository --yes --update ppa:ansible/ansible
