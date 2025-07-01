782f2e168c3f0350a1014615c51a2b6bd77639e1
Otto Lukacs <ottolukacs@ottolukacs.com>
2025-07-01 13:37:03 +0200

    Enhance deployment scripts by updating Docker Compose stack description and adding Wazuh cloning and cert generation tasks

--- iac/jumpstart.sh
+++ iac/jumpstart.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Set hostname
 hostnamectl set-hostname blocks-srv
+
+# Update repositories and install dependencies
 apt update
 apt install software-properties-common -y
+
+# Add Ansible PPA and install Ansible
 add-apt-repository --yes --update ppa:ansible/ansible
 apt install ansible -y
+
+# Install Ansible collections
+ansible-galaxy collection install community.docker
