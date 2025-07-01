1d3255c0139e2c0daa4a5e294dd793e09ccdb20c
Otto Lukacs <ottolukacs@ottolukacs.com>
2025-07-01 16:21:59 +0200

    Refactor security group rules to rename HTTP rule to HTTPS and update port settings

--- iac/SecGrp.tf
+++ iac/SecGrp.tf
@@ -15,12 +15,12 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   to_port           = 22
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
   security_group_id = aws_security_group.blocks-sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
+  from_port         = 443
   ip_protocol       = "tcp"
-  to_port           = 80
+  to_port           = 443
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
